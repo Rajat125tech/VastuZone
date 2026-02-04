d1bad7430ab546e101c4b24eb55276891c42d090
Rajat125tech <srivastava.rajat2004@gmail.com>
2026-02-05 00:05:34 +0530

    Update API endpoint to use production URL

--- vastuzone-frontend/src/components/Navbar.js
+++ vastuzone-frontend/src/components/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
 
       try {
         const res = await fetch(
-          `http://localhost:5001/api/users/me/${currentUser.uid}`
+          `https://vastuzone-backend.onrender.com/api/users/me/${currentUser.uid}`
         );
         const data = await res.json();
         setRole(data.role); // "user" or "expert"
