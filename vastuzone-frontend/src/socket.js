08f925ffd69143d770fdfd5cec7d6495dde7b386
Rajat125tech <srivastava.rajat2004@gmail.com>
2026-02-10 03:38:35 +0530

    Update socket.js

--- vastuzone-frontend/src/socket.js
+++ vastuzone-frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5001";
+  "https://vastuzone-backend.onrender.com" || "http://localhost:5001";
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
